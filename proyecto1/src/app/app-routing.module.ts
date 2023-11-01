8233f05e4063fb70ae45c308d2858570e9ca74f8
Sibila <sibila.araujo@correo.ucu.edu.uy>
2023-11-01 08:42:24 -0300

    nuevos path

--- proyecto1/src/app/app-routing.module.ts
+++ proyecto1/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { NombreJugadorComponent } from './components/Jugador/nombre-jugador/nombre-jugador.component';
 import { CrearComponent } from './components/Juego/crear/crear.component';
 import { LoginComponent } from './components/Juego/login/login.component';
+import { CrearActividadComponent } from './components/crear-actividad/crear-actividad.component';
+import { CrearPropuestaComponent } from './components/crear-propuesta/crear-propuesta.component';
+import { CrearJuegoComponent } from './components/crear-juego/crear-juego.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -12,6 +15,10 @@ const routes: Routes = [
   { path: 'nombreJugador', component: NombreJugadorComponent },
   { path: 'login', component: LoginComponent },
   { path: 'crear', component: CrearComponent },
+  { path: 'crearActividad', component: CrearActividadComponent},
+  { path: 'crearPropuesta', component: CrearPropuestaComponent},
+  { path: 'crearJuego', component: CrearJuegoComponent}
+  
 ];
 
 @NgModule({
