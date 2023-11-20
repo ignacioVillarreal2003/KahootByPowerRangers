e70a0c911c13fa448d32d7fe0b9f6ed2573d926a
IgnacioVillarreal31 <ignacio.villarreal@correo.ucu.edu.uy>
2023-11-20 13:58:49 -0300

    update

--- proyecto1/src/app/components/Juego/Play/actividades-mas-votadas/actividades-mas-votadas.component.ts
+++ proyecto1/src/app/components/Juego/Play/actividades-mas-votadas/actividades-mas-votadas.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SocketService } from 'src/app/components/services/socket.service';
+import { AdminService } from 'src/app/components/services/HTTPServices/admin.service';
+import { DatosJuegoService } from 'src/app/components/services/datos-juego.service';
 
 @Component({
   selector: 'app-actividades-mas-votadas',
@@ -7,9 +9,24 @@ import { SocketService } from 'src/app/components/services/socket.service';
   styleUrls: ['./actividades-mas-votadas.component.css']
 })
 export class ActividadesMasVotadasComponent {
-  constructor(private socketService: SocketService) {}
+  constructor(private socketService: SocketService, private adminService: AdminService, private datosJuegoService: DatosJuegoService) {}
 
   ngOnInit() {
     this.socketService.cerrarSocket();
+    this.obtenerVotos()
+  }
+
+  listaTopCalificaciones: any[] = [];
+
+  obtenerVotos(){
+    this.adminService.topCalificaciones(this.datosJuegoService.pin).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.listaTopCalificaciones = response;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
   }
 }
